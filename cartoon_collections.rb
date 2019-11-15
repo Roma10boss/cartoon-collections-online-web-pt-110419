d8b0ed1f250286679bf06ee8214bc8fe5d9bb564
Romario Gustave <romagustave10@gmail.com>
2019-11-15 06:37:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)
   # Your code here
-  array =[]
-  yeild array
+  array =["Doc", "Dopey", "Bashful", "Grumpy"]
+  yeild array.join
 end
 
 def summon_captain_planet# code an argument here
